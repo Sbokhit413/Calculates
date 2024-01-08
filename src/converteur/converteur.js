914a5bac8015104773d483a6fdecff17a9e24a9e
Lupitot <paul.piauger2@gmail.com>
2024-01-08 10:02:16 +0100

    ajout debut code converteur

---
+++ src/converteur/converteur.js
@@ -0,0 +1,19 @@
+
+
+
+
+function converteur(format, value){
+    if(format == "bin"){
+        converteurBin(value);
+    } else if(format == "hex"){
+        converteurHex(value);
+    }
+}
+
+function converteurBin(value){
+
+}
+
+function converteurHex(value){
+
+}
