469cea93aa5c9497cb6f26421fdfce2a3ad3b2ad
Lupitot <paul.piauger2@gmail.com>
2024-01-08 10:21:33 +0100

    converteur op

--- src/converteur/converteur.js
+++ src/converteur/converteur.js
@@ -1,19 +1,11 @@
 
+var getElem = document.getElementById("result-Bin");
+var getElem2 = document.getElementById("result-Oct");
+var getElem3 = document.getElementById("result-Hex");
 
-
-
-function converteur(format, value){
-    if(format == "bin"){
-        converteurBin(value);
-    } else if(format == "hex"){
-        converteurHex(value);
-    }
-}
-
-function converteurBin(value){
-
-}
-
-function converteurHex(value){
-
+function converteur() {
+    var number = document.getElementById("input").value;
+    getElem.innerHTML = parseInt(number, 10).toString(2);
+    getElem2.innerHTML = parseInt(number, 10).toString(8);
+    getElem3.innerHTML = parseInt(number, 10).toString(16);
 }
